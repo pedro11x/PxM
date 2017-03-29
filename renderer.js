c448fe0204070f5b1d904765c0168a1b3dd4ebf4
pedro11x <a.pacheco@campus.fct.unl.pt>
2017-03-29 21:45:10 +0100

    changed style, added pbm support, fixed ppm

--- renderer.js
+++ renderer.js
@@ -8,7 +8,9 @@ function createContext(w, h){
 document.getElementById('file-selector').addEventListener('change', function(e){
     let file = e.target.files[0];
     if(file){
+        /*Updating progress*/progress('imgloadprogress', 5);
         readTextFile(file).then((text)=>{
+            /*Updating progress*/progress('imgloadprogress', 15);
             readTextAndRender(text);
         }).catch(console.error);
     }
@@ -41,6 +43,9 @@ function readTextAndRender(text){
     let heigth = header.height;
     console.log('PXM file read. The following attributes were found:', header);
     window.ctx = createContext(width, heigth);
+    
+    /*Updating progress*/progress('imgloadprogress', 20);
+    
     render(ctx, width, heigth, data, type);
 }
 
@@ -60,23 +65,30 @@ function render(ctx, width, height, data, type){
     
     ctx.putImageData(img, 0, 0);
     console.info('Rendering has finished.', img);
+    /*Updating progress*/progress('imgloadprogress', 100);
 }
 
 function renderPPM(img, width, height, data){
     let ppmcomponents = 3;
     let imgcomponents = 4;
+    let npixels = (width * height);
     
-    for(let i = 0; i < (width * height); i++){
+    for(let i = 0; i < npixels; i++){
         let ppmoffset = i * ppmcomponents;
         let imgoffset = i * imgcomponents;
         img.data[imgoffset + 0] = data[ppmoffset + 0];
         img.data[imgoffset + 1] = data[ppmoffset + 1];
         img.data[imgoffset + 2] = data[ppmoffset + 2];
         img.data[imgoffset + 3] = 255; //overriding alpha
+        
+        /*Updating progress*/
+        if((i%(Math.round(npixels/10)))==0){
+            progress('imgloadprogress', 20 + Math.round(80 * (i/npixels)));
+        }
     }
 }
 
-function renderPPM(img, width, height, data){
+function renderPGM(img, width, height, data){
     let imgcomponents = 4;
     
     for(let i = 0; i < (width * height); i++){
@@ -96,4 +108,24 @@ function readHeader(data){
         height: data.shift(),
         depth: data.shift()
     };
+}
+
+function progress(id, value){
+    window['pb'+id] = window['pb'+id] || {values: [], interval:null};
+    window['pb'+id].values.push(value);
+    window['pb'+id].interval = window['pb'+id].interval || setInterval(()=>{
+        let pb = document.getElementById(id);
+        let next = window['pb'+id].values.shift();
+        if(next && next < 100){
+            if(pb.childNodes.length > 0){
+                let span = pb.childNodes[0];
+                span.style.width = next+'%';
+                console.log(next);
+            }else
+                pb.innerHTML = '<span style="width:'+next+'%"></span>';
+            pb.classList.remove('done');
+        }else{
+            pb.classList.add('done');
+        }
+    }, 100);
 }
